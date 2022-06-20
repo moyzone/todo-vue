132d7674806970bb95217c23a68418b180fb81a4
Sourav Das <moyzone108@gmail.com>
2022-06-20 17:22:26 +0530

    first commit

---
+++ src/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Hey this is Home</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  
+  data() {
+    
+  },
+  
+};
+</script>
+
+<style scoped>
+
+</style>
